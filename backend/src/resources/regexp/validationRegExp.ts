c397b6e16f2e375d3ddc3312509ed25c81fe955f
geozi <yorgosaz@gmail.com>
2025-04-21 12:08:02 +0300

    refactor: Removed TICKET_ID_REGEX from validationRegExp.

--- backend/src/resources/regexp/validationRegExp.ts
+++ backend/src/resources/regexp/validationRegExp.ts
@@ -1,6 +1,3 @@
 export const PHONE_NUMBER_REGEX = new RegExp(/^\d{10}$/);
 
 export const CITY_REGEX = new RegExp(/^[A-Za-z\s]+$/);
-
-// EMP -DATE(YYY/MM/DD) - RN - 6 DIGITS
-export const TICKET_ID_REGEX = new RegExp(/^EMP\d{8}RN\d{6}$/);
