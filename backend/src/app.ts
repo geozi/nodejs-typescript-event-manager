d8ba29c8a2c3b16823208ae810a370d9a43099eb
geozi <yorgosaz@gmail.com>
2025-04-22 13:11:57 +0300

    mix: Added types/express dependency; added nodemon and database connection functionality to app.ts.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,20 @@
+import express from "express";
 import "reflect-metadata";
+import { AppDataSource } from "./dataSource";
+const app = express();
+const port = 3000;
+
+async function main() {
+  try {
+    await AppDataSource.initialize();
+    console.log("Connection to database established");
+  } catch (error) {
+    console.log("Failed to connect to database", error);
+  }
+}
+
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
+
+main();
