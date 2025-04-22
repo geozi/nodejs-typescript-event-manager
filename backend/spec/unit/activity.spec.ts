75206090186f5ddcd669a2b5efc113ab98dadbfa
geozi <yorgosaz@gmail.com>
2025-04-22 11:30:43 +0300

    spec: Added missing field to mock object in activity.spec test suite.

--- backend/spec/unit/activity.spec.ts
+++ backend/spec/unit/activity.spec.ts
@@ -20,6 +20,7 @@ describe("Activity entity validation tests", () => {
       mockActivity.activityType = validActivityInputs.activityType;
       mockActivity.createdAt = validCommonInputs.createdAt;
       mockActivity.updatedAt = validCommonInputs.updatedAt;
+      mockActivity.events = validCommonInputs.events;
     });
 
     it("activity has valid inputs", () => {
@@ -31,6 +32,7 @@ describe("Activity entity validation tests", () => {
         ...validActivityInputs,
         createdAt: validCommonInputs.createdAt,
         updatedAt: validCommonInputs.updatedAt,
+        events: validCommonInputs.events,
       });
     });
   });
