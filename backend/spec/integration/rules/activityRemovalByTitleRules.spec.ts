a61523069341bbe14aaaa5e038113d1096f746f9
geozi <yorgosaz@gmail.com>
2025-04-26 20:48:12 +0300

    spec: Added activityRemovalByTitleRules.spec test suite.

---
+++ backend/spec/integration/rules/activityRemovalByTitleRules.spec.ts
@@ -0,0 +1,162 @@
+import { Request, Response } from "express";
+import { activityFailedValidation } from "messages/validation/activityValidationMessages";
+import { catchExpressValidationErrors } from "middleware/catchers/expressErrorCatcher";
+import { activityRemovalByTitleRules } from "middleware/rules/activityRules";
+import { httpCodes } from "resources/codes/httpStatusCodes";
+import sinon, { SinonSpy, SinonStub } from "sinon";
+import { invalidActivityInputs, validActivityInputs } from "spec/testInputs";
+
+describe("Activity removal by title rules: integration tests", () => {
+  let req: Partial<Request>;
+  let res: Partial<Response>;
+  let next: SinonSpy;
+  let statusStub: SinonStub;
+  let jsonSpy: SinonSpy;
+  let mockTitle: string;
+  let mockNumericValue: number;
+  const activityRemovalArray = [
+    ...activityRemovalByTitleRules(),
+    catchExpressValidationErrors,
+  ];
+
+  describe("Positive scenario", () => {
+    beforeEach(() => {
+      // Reset stubs and spies
+      sinon.restore();
+
+      // Stubs and spies
+      res = {
+        status: sinon.stub().callsFake(() => {
+          return res;
+        }) as unknown as SinonStub,
+        json: sinon.spy(),
+      };
+      next = sinon.spy();
+
+      // Mocks
+      mockTitle = validActivityInputs.title;
+
+      // HTTP request
+      req = { body: JSON.parse(JSON.stringify({ title: mockTitle })) };
+    });
+
+    it("request has valid title", async () => {
+      for (const middleware of activityRemovalArray) {
+        await middleware(req as Request, res as Response, next);
+      }
+
+      statusStub = res.status as SinonStub;
+      jsonSpy = res.json as SinonSpy;
+
+      expect(statusStub.notCalled).toBeTrue();
+      expect(jsonSpy.notCalled).toBeTrue();
+    });
+  });
+
+  describe("Negative scenarios", () => {
+    beforeEach(() => {
+      // Reset stubs and spies
+      sinon.restore();
+
+      // Stubs and spies
+      res = {
+        status: sinon.stub().callsFake(() => {
+          return res;
+        }) as unknown as SinonStub,
+        json: sinon.spy(),
+      };
+      next = sinon.spy();
+
+      // Mocks
+      mockTitle = validActivityInputs.title;
+      mockNumericValue = 1;
+
+      // HTTP request
+      req = { body: JSON.parse(JSON.stringify({ title: mockTitle })) };
+    });
+
+    it("title is undefined", async () => {
+      req.body.title = undefined;
+
+      for (const middleware of activityRemovalArray) {
+        await middleware(req as Request, res as Response, next);
+      }
+
+      statusStub = res.status as SinonStub;
+      jsonSpy = res.json as SinonSpy;
+
+      expect(statusStub.calledWith(httpCodes.BAD_REQUEST)).toBeTrue();
+      expect(
+        jsonSpy.calledWith({
+          errors: [
+            { message: activityFailedValidation.TITLE_REQUIRED_MESSAGE },
+          ],
+        })
+      ).toBeTrue();
+    });
+
+    it("title is not a string", async () => {
+      req.body.title = mockNumericValue;
+
+      for (const middleware of activityRemovalArray) {
+        await middleware(req as Request, res as Response, next);
+      }
+
+      statusStub = res.status as SinonStub;
+      jsonSpy = res.json as SinonSpy;
+
+      expect(statusStub.calledWith(httpCodes.BAD_REQUEST)).toBeTrue();
+      expect(
+        jsonSpy.calledWith({
+          errors: [
+            { message: activityFailedValidation.TITLE_INVALID_TYPE_MESSAGE },
+          ],
+        })
+      ).toBeTrue();
+    });
+
+    it("title is too short", async () => {
+      req.body.title = invalidActivityInputs.TITLE_TOO_SHORT;
+
+      for (const middleware of activityRemovalArray) {
+        await middleware(req as Request, res as Response, next);
+      }
+
+      statusStub = res.status as SinonStub;
+      jsonSpy = res.json as SinonSpy;
+
+      expect(statusStub.calledWith(httpCodes.BAD_REQUEST)).toBeTrue();
+      expect(
+        jsonSpy.calledWith({
+          errors: [
+            {
+              message: activityFailedValidation.TITLE_BELOW_MIN_LENGTH_MESSAGE,
+            },
+          ],
+        })
+      ).toBeTrue();
+    });
+
+    it("title is too long", async () => {
+      req.body.title = invalidActivityInputs.TITLE_TOO_LONG;
+
+      for (const middleware of activityRemovalArray) {
+        await middleware(req as Request, res as Response, next);
+      }
+
+      statusStub = res.status as SinonStub;
+      jsonSpy = res.json as SinonSpy;
+
+      expect(statusStub.calledWith(httpCodes.BAD_REQUEST)).toBeTrue();
+      expect(
+        jsonSpy.calledWith({
+          errors: [
+            {
+              message: activityFailedValidation.TITLE_ABOVE_MAX_LENGTH_MESSAGE,
+            },
+          ],
+        })
+      ).toBeTrue();
+    });
+  });
+});
