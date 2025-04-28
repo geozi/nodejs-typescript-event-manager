095405b69d6b7d3d02efd6d16af3c233450c6fb5
geozi <yorgosaz@gmail.com>
2025-04-28 11:19:53 +0300

    spec: Added commonMapper.spec test suite.

---
+++ backend/spec/unit/commonMapper.spec.ts
@@ -0,0 +1,44 @@
+import { Request } from "express";
+import { reqToId } from "mappers/commonMapper";
+
+describe("Common mapper unit tests", () => {
+  let req: Partial<Request>;
+  let mockInput: number | string;
+
+  describe(`${reqToId.name}`, () => {
+    it("request has valid id", () => {
+      mockInput = 1;
+
+      req = {
+        body: JSON.parse(
+          JSON.stringify({
+            id: mockInput,
+          })
+        ),
+      };
+
+      const id = reqToId(req as Request);
+
+      expect(id).toBeInstanceOf(Number);
+      expect(id).toEqual(1);
+    });
+
+    it("id is a string -> TypeError", () => {
+      mockInput = "680f398b117c19aabf04217d";
+
+      req = {
+        body: JSON.parse(
+          JSON.stringify({
+            id: mockInput,
+          })
+        ),
+      };
+
+      try {
+        reqToId(req as Request);
+      } catch (error) {
+        expect(error).toBeInstanceOf(TypeError);
+      }
+    });
+  });
+});
